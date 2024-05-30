cb36516e125380777e57aa4f1bb2b96e0cbcaaa7
Zachti <114566720+Zachti@users.noreply.github.com>
2024-05-31 00:16:59 +0300

    built GameData class

--- GameData.cs
+++ GameData.cs
@@ -1,5 +1,9 @@
+ using System.Collections.Generic;
+using System;
+
  public class GameData
     {
+        private static readonly Random m_Random = new Random();
         public Player PlayerOne { get; }
         public Player PlayerTwo { get; }
         public Player CurrentPlayer { get; set; }
@@ -7,13 +11,66 @@
         public int BoardHeight { get; set; }
         public int BoardWidth { get; set; }
 
-        public GameData(Player playerOne, Player playerTwo, int boardWidth, int boardHeight)
+        public GameData(Player i_PlayerOne, Player i_PlayerTwo, int i_BoardWidth, int i_BoardHeight)
         {
-            PlayerOne = playerOne;
-            PlayerTwo = playerTwo;
-            CurrentPlayer = playerOne;
-            BoardWidth = boardWidth;
-            BoardHeight = boardHeight;
+            PlayerOne = i_PlayerOne;
+            PlayerTwo = i_PlayerTwo;
+            CurrentPlayer = i_PlayerOne;
+            BoardWidth = i_BoardWidth;
+            BoardHeight = i_BoardHeight;
             Letters = new BoardLetter[BoardHeight, BoardWidth];
         }
+        
+        public void InitializeBoard()
+        {
+            char[] boardLetters = initializeBoardLetters();
+            List<Cell> randomCells = getRandomCellsList();
+
+            foreach(char letter in boardLetters)
+            {
+                int randomSelection = GetRandomNumber(0, randomCells.Count);
+                Cell firstCell = randomCells[randomSelection];
+
+                randomSelection = GetRandomNumber(0, randomCells.Count);
+                Cell secondCell = randomCells[randomSelection];
+
+                randomCells.Remove(firstCell);
+                randomCells.Remove(secondCell);
+
+                Letters[firstCell.Row, firstCell.Column] = new BoardLetter(letter);
+                Letters[secondCell.Row, secondCell.Column] = new BoardLetter(letter);
+            }
+        }
+        
+        private List<Cell> getRandomCellsList()
+        {
+            List<Cell> randomCells = new List<Cell>(BoardHeight * BoardWidth);
+
+            for (int i = 0; i < BoardHeight; i++)
+            {
+                for (int j = 0; j < BoardWidth; j++)
+                {
+                   randomCells.Add(new Cell(i, j));
+                }
+            }
+
+            return randomCells;
+        }
+        
+        private char[] initializeBoardLetters()
+        {
+            char[] boardLetters = new char[BoardHeight * BoardWidth / 2];
+
+            for(int i = 0; i < boardLetters.Length; i++)
+            {
+                boardLetters[i] = (char)('A' + i);
+            }
+
+            return boardLetters;
+        }
+  
+        private int GetRandomNumber(int i_RangeStart, int i_RangeEnd)
+        { 
+            return m_Random.Next(i_RangeStart, i_RangeEnd);
+        } 
     }
