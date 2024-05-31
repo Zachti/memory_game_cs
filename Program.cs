58b2f54048222339807a71be9a02052b662089f3
Zachti <114566720+Zachti@users.noreply.github.com>
2024-05-31 15:32:49 +0300

    created static ConfigureServices method in program class

--- Program.cs
+++ Program.cs
@@ -4,19 +4,24 @@
     {
         public static void Main(string[] i_Args)
         {
-        var ServiceProvider = new ServiceCollection()
-        .AddSingleton<IMenu, Menu>()
-        .AddTransient<GameUIManager>()
-        .AddSingleton<IGameData, GameData>()
-        .AddTransient<GameManager>()
-        .AddSingleton<Player>(provider => new Player("Player One", ePlayerTypes.Human))
-        .AddSingleton<Player>(provider => new Player("Player Two", ePlayerTypes.AI))
-        .AddSingleton<Board>(provider => new Board(6, 6))
-        .AddSingleton<IGameMode>(provider => new GameMode(eGameModes.singlePlayer))
-        .BuildServiceProvider();
+            var ServiceProvider = ConfigureServices();
 
-        var gameUiManager = ServiceProvider.GetRequiredService<GameUIManager>();
+            var gameUiManager = ServiceProvider.GetRequiredService<GameUIManager>();
 
-        gameUiManager.StartGame();
+            gameUiManager.StartGame();
         }
-    }
+
+        private static IServiceProvider ConfigureServices()
+        {
+            return new ServiceCollection()
+                .AddSingleton<IMenu, Menu>()
+                .AddTransient<GameUIManager>()
+                .AddSingleton<IGameData, GameData>()
+                .AddTransient<GameManager>()
+                .AddSingleton<Player>(provider => new Player("Player One", ePlayerTypes.Human))
+                .AddSingleton<Player>(provider => new Player("Player Two", ePlayerTypes.AI))
+                .AddSingleton<Board>(provider => new Board(6, 6))
+                .AddSingleton<IGameMode>(provider => new GameMode(eGameModes.singlePlayer))
+                .BuildServiceProvider();
+        }   
+}
